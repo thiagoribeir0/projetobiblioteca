f0d351fe8ee85227fe56b9059cbcc19f3cd636bf
cauana sallviano <cauanasallviano@edu.uniube.br>
2024-04-03 20:01:46 -0300

    ref css pag login

--- login.php
+++ login.php
@@ -23,6 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
     <h2>Login</h2>
