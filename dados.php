3075f775dba1770232cf9f1c79c097523a2a2afd
Thiago <thiagobarbosaribeiro@edu.uniube.br>
2024-04-02 23:07:11 -0300

    teste login true

---
+++ dados.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["usuario_logado"])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bem vinde!</h1>
+</body>
+</html>
