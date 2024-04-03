468c5c485946e0e3b2ae87e0639b45e1b297a488
Thiago <thiagobarbosaribeiro@edu.uniube.br>
2024-04-02 22:40:29 -0300

    página de cadastro

--- cadastro.php
+++ cadastro.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Cadastro</title>
 </head>
 <body>
-    <h2>Login</h2>
-    <form action="login.php" method="post">
-        
+    <h2>Cadastro</h2>
+    <form action="cadastro.php" method="post">
+
         <label for="username">Usuário:</label>
         <input type="text" id="username" name="username" required>
         
@@ -16,14 +16,9 @@
         
         <label for="password">Senha:</label>
         <input type="password" id="password" name="password" required>
-        
         <br><br>
         
-        <input type="submit" value="Entrar">
+        <input type="submit" value="Cadastrar">
     </form>
-    
-    <p>
-        Não tem uma conta? <a href="cadastro.php">Faça seu cadastro</a>.</p>
-
 </body>
 </html>
